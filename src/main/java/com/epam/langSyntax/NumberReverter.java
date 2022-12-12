d5eace7c7cdb92789c6999024c84c8ddcd9e5977
= <=>
2022-12-12 20:28:06 +0400

    Easy.

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,13 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        String result = "";
+        while(number != 0){
+            int lastDigit = number % 10;
+            result += lastDigit + "";
+            number /= 10;
+        }
+        System.out.println(Integer.parseInt(result));
     }
 
 }
